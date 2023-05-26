2c633e2c568cca77d6b6b783dbc856e8bc6a3813
harsh.flairlabs <harsh.flairlabs@gitlab.idfy.com>
2023-05-26 12:32:30 +0530

    changed again

--- src/Connector.jsx
+++ src/Connector.jsx
@@ -7,61 +7,58 @@ const Connector = (props) => {
   const [attrs, setter] = useState({});
   const [children, setChildren] = useState(props.node.children);
   const subscribed = useRef({});
-  const bindingsChanged = useRef(false);
-
-  const updateAttributes = () => {
-    const pathAttrMap = _.invert(props.node.bindings);
-    const childBoundProps = _.keys(props.node.bindings);
-
-    const groups = _.uniq(
-      _.map(childBoundProps, (curr) => props.node.bindings[curr].split("::")[0])
-    );
-
-    const toBeUnsubscribed = _.differenceBy(_.keys(subscribed.current), groups);
-
-    _.forEach(toBeUnsubscribed, (key) => {
-      unsubscribe(subscribed.current[key]);
-      delete subscribed.current[key];
-    });
-
-    _.forEach(groups, (key) => {
-      if (!subscribed.current[key]) {
-        subscribed.current[key] = subscribe(key, {
-          next: (value) => {
-            setter((prevAttrs) =>
-              _.reduce(
-                pathAttrMap,
-                (prev, attribute, path) => {
-                  const [storeKey, storePath] = path.split("::");
-                  const newValue = getValue(storeKey, storePath);
-                  if (storeKey === key && attribute !== "children") {
-                    _.set(prev, attribute, newValue);
-                  }
-
-                  if (storeKey === key && attribute === "children") {
-                    setChildren(newValue);
-                  }
-
-                  return prev;
-                },
-                { ...prevAttrs }
-              )
-            );
-          },
-        });
-      }
-    });
-  };
 
   useEffect(() => {
-    if (bindingsChanged.current) {
-      updateAttributes();
-      bindingsChanged.current = false;
-    }
-  }, [props.node.bindings]);
+    const updateAttributes = () => {
+      const pathAttrMap = _.invert(props.node.bindings);
+      const childBoundProps = _.keys(props.node.bindings);
 
-  useEffect(() => {
-    bindingsChanged.current = true;
+      const groups = _.uniq(
+        _.map(childBoundProps, (curr) =>
+          props.node.bindings[curr].split("::")[0]
+        )
+      );
+
+      const toBeUnsubscribed = _.differenceBy(
+        _.keys(subscribed.current),
+        groups
+      );
+
+      _.forEach(toBeUnsubscribed, (key) => {
+        unsubscribe(subscribed.current[key]);
+        delete subscribed.current[key];
+      });
+
+      _.forEach(groups, (key) => {
+        if (!subscribed.current[key]) {
+          subscribed.current[key] = subscribe(key, {
+            next: (value) => {
+              setter((prevAttrs) =>
+                _.reduce(
+                  pathAttrMap,
+                  (prev, attribute, path) => {
+                    const [storeKey, storePath] = path.split("::");
+                    const newValue = getValue(storeKey, storePath);
+                    if (storeKey === key && attribute !== "children") {
+                      _.set(prev, attribute, newValue);
+                    }
+
+                    if (storeKey === key && attribute === "children") {
+                      setChildren(newValue);
+                    }
+
+                    return prev;
+                  },
+                  { ...prevAttrs }
+                )
+              );
+            },
+          });
+        }
+      });
+    };
+
+    updateAttributes();
   }, [props.node.bindings]);
 
   const childRef = useRef();
