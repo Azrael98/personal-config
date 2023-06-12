03ba543cebea325589883f1d85a3a3c18f0de4e9
harsh.flairlabs <harsh.flairlabs@gitlab.idfy.com>
2023-06-12 12:01:36 +0530

    updated config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'config-renderer.js',
+    filename: 'index.js',
     library: 'config-renderer',
     libraryTarget: 'umd',
   },
